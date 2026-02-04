01829245c7a635fbc1937f52b625aaf75e56aa22
Perido, Maria Cristina <20162526@s.ubaguio.edu>
2026-02-04 13:31:53 +0800

    Perido new-app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent  } from './parts/home/home.component';
+import { PaymentInfoComponent } from './AcctSettings/payment-info/payment-info.component'; //automatic na meron
+import { UserBioComponent } from './AcctSettings/user-bio/user-bio.component'; // automatic na meron
+import { UserProfileComponent } from './AcctSettings/user-profile/user-profile.component'; //automatic na meron
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'home',
+    component: HomeComponent,
+
+},
+{
+    path: 'profile',
+    component: UserProfileComponent,
+},
+{
+    path: 'payment',
+    component: PaymentInfoComponent,
+},
+{
+    path: 'user',
+    component: UserBioComponent,
+}
+];
---
+++ src/app/parts/navi/navi.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-navi',
+  imports: [RouterLink],
+  templateUrl: './navi.component.html',
+  styleUrl: './navi.component.css'
+})
+export class NaviComponent {
+  author:string ="Perido";
+}
